009d3b4ea27c64f37d89581570dd06e5a47a47ca
Manan_1710 <mananpatel17106@gmail.com>
2026-07-16 00:16:21 +0530

    fix: update getLikedVideos to exclude null video entries and improve response structure

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -94,57 +94,62 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
 
 const getLikedVideos = asyncHandler(async (req, res) => {
   const likedVideos = await Like.aggregate([
-  {
-    $match: {
-      likedBy: new mongoose.Types.ObjectId(req.user._id),
-    },
-  },
-  {
-    $lookup: {
-      from: "videos",
-      localField: "video",
-      foreignField: "_id",
-      as: "video",
-      pipeline: [
-        {
-          $lookup: {
-            from: "users",
-            localField: "owner",
-            foreignField: "_id",
-            as: "owner",
-            pipeline: [
-              {
-                $project: {
-                  fullName: 1,
-                  username: 1,
-                  avatar: 1,
-                },
-              },
-            ],
-          },
+    {
+      $match: {
+        likedBy: new mongoose.Types.ObjectId(req.user._id),
+        video: {
+          $ne: null,
         },
-        {
-          $addFields: {
-            owner: {
-              $first: "$owner",
-            },
-          },
-        },
-      ],
-    },
-  },
-  {
-    $addFields: {
-      video: {
-        $first: "$video",
       },
     },
-  },
-]);
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "video",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner",
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        video: {
+          $first: "$video",
+        },
+      },
+    },
+  ]);
 
   return res
     .status(200)
-    .json(new ApiError(200, likedVideos, "Liked videos fetched successfully"));
+    .json(
+      new ApiResponse(200, likedVideos, "Liked videos fetched successfully")
+    );
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
