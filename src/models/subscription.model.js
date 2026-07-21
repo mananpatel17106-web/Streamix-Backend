0297ca533afcc7a552cb6020a457f183c9dcdfe4
Manan_1710 <mananpatel17106@gmail.com>
2026-07-21 19:13:05 +0530

    refactor: improve formatting and structure of subscription model schema

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,14 +1,40 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new Schema({
-  subscriber : {
-    type : Schema.Types.ObjectId,   //one who is subscribing
-    ref : "User"
+const subscriptionSchema = new Schema(
+  {
+    subscriber: {
+      type: Schema.Types.ObjectId, // User who subscribes
+      ref: "User",
+      required: true,
+    },
+
+    channel: {
+      type: Schema.Types.ObjectId, // Channel being subscribed to
+      ref: "User",
+      required: true,
+    },
   },
-  channel : {
-    type : Schema.Types.ObjectId,   //one to whom 'subscriber' is subscribing
-    ref : "User"
+  {
+    timestamps: true,
   }
-},{timestamps:true})
+);
 
-export const Subscription = mongoose.model("Subscription",subscriptionSchema)
+// Prevent duplicate subscriptions
+subscriptionSchema.index(
+  {
+    subscriber: 1,
+    channel: 1,
+  },
+  {
+    unique: true,
+  }
+);
+
+// Faster queries
+subscriptionSchema.index({ subscriber: 1 });
+subscriptionSchema.index({ channel: 1 });
+
+export const Subscription = mongoose.model(
+  "Subscription",
+  subscriptionSchema
+);
